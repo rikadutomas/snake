8398dff14bf99d7288ab15c715030fcf4b06e31e
rikadutomas <39969195+rikadutomas@users.noreply.github.com>
2024-12-29 18:04:08 +0000

    Another update

--- snake2.py
+++ snake2.py
@@ -2,6 +2,7 @@ import pygame,sys,random
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
+SNAKE_SIZE = 20
 FPS = 10
 
 BLUE = 'blue'
@@ -18,10 +19,11 @@ class Game:
         self.clock = pygame.time.Clock()
         self.fps = FPS
         self.running = True
-        pygame.draw.rect(self.screen,WHITE,(0,0,WINDOW_WIDTH,WINDOW_HEIGHT),1)
-        self.board = pygame.draw.rect(self.screen,WHITE,(10,100,WINDOW_WIDTH-20,WINDOW_HEIGHT - 120),1)
-        self.board_center = self.board.center
+        self.board = (11,101,WINDOW_WIDTH-22,WINDOW_HEIGHT - 122)
+        # pygame.draw.rect(self.screen,WHITE,(0,0,WINDOW_WIDTH,WINDOW_HEIGHT),1)
+        pygame.draw.rect(self.screen,WHITE,(10,100,WINDOW_WIDTH-20,WINDOW_HEIGHT-120),1)
         self.snake_group = pygame.sprite.Group()
+        self.person_group = pygame.sprite.Group()
         self.direction = 'RIGHT'
         
     def events(self):
@@ -38,14 +40,18 @@ class Game:
                     self.direction = 'LEFT'
                 if event.key == pygame.K_RIGHT:
                     self.direction = 'RIGHT'
-        
 
     def run(self):
         snake = Snake(self)
+        self.person = Person(self)
         self.snake_group.add(snake)
+        # self.person_group.add(person)
+        
         while self.running:
+            self.screen.fill(BLUE,self.board,0)
             self.events()
             self.snake_group.update(self.direction)
+            self.person_group.draw(self.screen)
             pygame.display.flip()
             self.clock.tick(self.fps)
             
@@ -54,14 +60,13 @@ class Snake(pygame.sprite.Sprite):
         super().__init__()
         self.x = 640
         self.y = 400
-        self.snake = pygame.draw.rect(game.screen,WHITE,((self.x,self.y),(20,20)))
-    
-    def update(self,direction):
+        self.rect = pygame.draw.rect(game.screen,WHITE,((self.x,self.y),(SNAKE_SIZE,SNAKE_SIZE)))
+        
+    def move(self,direction):
         match direction:
             case 'LEFT':
                 self.x -= 20
             case 'RIGHT':
-                print('match RIGHT')
                 self.x += 20
             case 'UP':
                 self.y -= 20
@@ -70,15 +75,30 @@ class Snake(pygame.sprite.Sprite):
             case _:
                 print('No MATCH')
         
-        pygame.draw.rect(game.screen,WHITE,((self.x,self.y),(20,20)))
-
+        self.rect.move_ip(self.x,self.y)
+        self.rect = pygame.draw.rect(game.screen,WHITE,((self.x,self.y),(SNAKE_SIZE,SNAKE_SIZE)))
+    
+    def check_collisions(self):
+        if pygame.sprite.spritecollide(self,game.person_group,True):
+            game.screen.fill(BLUE,(game.person.x,game.person.y,20,20),0)
+            game.person = Person(game)
+            
+            
+        
+    def update(self,direction):
+        self.move(direction)
+        self.check_collisions()
+        
 
 class Person(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self,game):
         super().__init__()
-        self.person = pygame.image.load('person2.png')
-
-
+        self.image = pygame.image.load('person2.png')
+        self.rect = self.image.get_rect()
+        self.x = random.randint(20,WINDOW_WIDTH - 40)
+        self.y = random.randint(100,WINDOW_HEIGHT-40)
+        self.rect.topleft = (self.x,self.y)
+        game.person_group.add(self)
 
 if __name__=='__main__':
     game = Game()  
